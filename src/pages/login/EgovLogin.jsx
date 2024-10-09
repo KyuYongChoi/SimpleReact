53f9befd17bb2842fd67a3a20ffd17242e74b528
CHOI KYU YONG <snowjmh@gmail.com>
2024-10-10 06:22:41 +0900

    ff

--- src/pages/login/EgovLogin.jsx
+++ src/pages/login/EgovLogin.jsx
@@ -1,21 +1,21 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import {Link} from 'react-router-dom';
 
 import EgovLoginContent from 'pages/login/EgovLoginContent';
 
 import URL from 'constants/url';
 
-function EgovLogin(props) {
-    console.group("EgovLogin");
-    console.log("[Start] EgovLogin ------------------------------");
-    console.log("EgovLogin [props] : ", props);
+function EgovLogin( props ) {
+    console.group( "EgovLogin" );
+    console.log( "[Start] EgovLogin ------------------------------" );
+    console.log( "EgovLogin [props] : ", props );
 
-    const onChangeLogin = (user) => {
-        props.onChangeLogin(user);
+    const onChangeLogin = ( user ) => {
+        props.onChangeLogin( user );
     }
 
-    console.log("------------------------------EgovLogin [End]");
-    console.groupEnd("EgovLogin");
+    console.log( "------------------------------EgovLogin [End]" );
+    console.groupEnd( "EgovLogin" );
 
     return (
         <div className="container">
@@ -23,7 +23,7 @@ function EgovLogin(props) {
                 {/* <!-- Location --> */}
                 <div className="location">
                     <ul>
-                        <li><Link to={URL.MAIN} className="home" >Home</Link></li>
+                        <li><Link to={URL.MAIN} className="home">Home</Link></li>
                         <li>로그인</li>
                     </ul>
                 </div>
